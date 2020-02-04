01ed2a83ad9b2588e7a83df8820cf5dbe3f0a9d0
Marco Cardoso <marco@vivoxie.com>
2020-02-04 16:10:32 -0600

    se sigue revisando forzado de densidad

--- mesh_c.py
+++ mesh_c.py
@@ -297,8 +297,8 @@ class mesh_C(mesh):
         I = 0
         a = np.longdouble(0)
         c = np.longdouble(0)
-        aa = np.longdouble(0.045)  #0.4
-        cc = np.longdouble(0.015)  #3.3
+        aa = np.longdouble(0.0645)  #0.4
+        cc = np.longdouble(0.0015)  #3.3
         linea_xi = 0.0
         linea_eta = 0.0
 
