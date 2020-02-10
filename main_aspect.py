6a3afd9146764a7c7e223f46481a424ba8eae01c
Marco Cardoso <marco@vivoxie.com>
2020-02-10 16:10:50 -0600

    se sigue revisando forzado de densidad

--- main_aspect.py
+++ main_aspect.py
@@ -26,10 +26,10 @@ densidad de puntos para la malla
 eje "XI"
 en el caso de malla tipo O, coincide con el número de puntos del perfil
 '''
-N = 60
+N = 11 # 60
 union = 6
 
-airfoil_points = 95
+airfoil_points = 35
 
 if malla == 'C':
     points = airfoil_points // 3 #* 2
