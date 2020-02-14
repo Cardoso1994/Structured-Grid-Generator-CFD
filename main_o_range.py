5c4e92c87cedf462c733532888e86ffa91258c21
Marco Cardoso <marcoacardosom@gmail.com>
2020-02-13 22:37:53 -0600

    graficas con acercamiento a sección deseadaw

--- main_o_range.py
+++ main_o_range.py
@@ -29,7 +29,7 @@ en el caso de malla tipo O, coincide con el número de puntos del perfil
 N = 45 # 60
 union = 6
 
-airfoil_points = 35
+airfoil_points = 45 # 35
 
 if malla == 'C':
     points = airfoil_points // 3 #* 2
@@ -46,10 +46,10 @@ R = 20 * c
 
 perfil = airfoil.NACA4(m, p, t, c)
 perfil.create_sin(points)
-flap = airfoil.NACA4(m, p, t, 0.2 * c, number=2)
-flap.create_sin(points)
-flap.rotate(-5)
-perfil.join(flap, dx=0.055, dy=0.05, union=union)
+# flap = airfoil.NACA4(m, p, t, 0.2 * c, number=2)
+# flap.create_sin(points)
+# flap.rotate(-5)
+# perfil.join(flap, dx=0.055, dy=0.05, union=union)
 perfil.rotate(0)
 
 if malla == 'O':
@@ -57,13 +57,28 @@ if malla == 'O':
 elif malla == 'C':
     mallaNACA = mesh_c.mesh_C(R, N, perfil)
 
-# mallaNACA.gen_Laplace(metodo='SOR')
-mallaNACA.gen_Poisson(metodo='SOR')
-print('after laplace')
 print('M = ' + str(mallaNACA.M))
 print('N = ' + str(mallaNACA.N))
 
-mallaNACA.plot()
+# mallaNACA.gen_Laplace(metodo='SOR')
+mallaNACA.gen_Poisson(metodo='SOR')
+print('after laplace')
+# mallaNACA.plot()
+limits = [[-1.0, 1.0, -0.5, 0.5], [-20.5, 20.5, -20.5, 20.5]]
+for limit in limits:
+    fig = plt.figure('malla')
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_xlim([limit[0], limit[1]])
+    ax.set_ylim([limit[2], limit[3]])
+    ax.set_aspect('equal')
+    # plt.axis('equal')
+    ax.plot(mallaNACA.X, mallaNACA.Y, 'k', linewidth=1.5)
+    ax.plot(mallaNACA.X[:, 0], mallaNACA.Y[:, 0], 'k', linewidth=1.9)
+
+    for i in range(mallaNACA.M):
+        ax.plot(mallaNACA.X[i, :], mallaNACA.Y[i, :], 'b', linewidth=1.5)
+    plt.draw()
+    plt.show()
 
 is_ok = False
 
