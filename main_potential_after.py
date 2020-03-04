694dc75da63b5d8db045a9e6be478ca1878783f2
Marco Cardoso <marco@vivoxie.com>
2020-03-04 14:19:16 -0600

    se continua escrito de flujo potencial

--- main_potential_after.py
+++ main_potential_after.py
@@ -19,8 +19,8 @@ from potential import potential_flow_o, potential_flow_o_esp, velocity,\
 import helpers
 
 # datos de perfil NACA
-m = 2  # combadura
-p_ = 4  # posicion de la combadura
+m = 0  # combadura
+p_ = 0  # posicion de la combadura
 t = 12  # espesor
 c = 1  # cuerda [m]
 
@@ -52,33 +52,74 @@ p0 = p_inf * (d0 / d_inf) ** gamma
 mach_inf = v_inf / c_inf
 Re = v_inf * d_inf / mu
 
-path = '/home/cardoso/'
-direc = 'six'
-mallaNACA = helpers.from_txt_mesh(filename='./potential_2412/' + direc
-                                  + '/mallaNACA.txt_mesh')
-phi = np.genfromtxt('./potential_2412/' + direc + '/phi.csv', delimiter=',')
-# f = open("./potential_2412/five/C.csv", "r")
-C = np.genfromtxt('./potential_2412/' + direc + '/C.csv', delimiter=',')
-theta = np.genfromtxt('./potential_2412/' + direc + '/theta.csv',
+path = './potential_final/'
+alfas = ['-4', '-2', '0', '2', '4', '6', '8', '10']
+alfas = ['-4', '0', '4', '8']
+
+map_ = 'viridis'
+cps = np.zeros((149, 4))
+j = 0
+for alfa in alfas:
+    mallaNACA = helpers.from_txt_mesh(filename=path
+                                  + '/mallaNACA_' + alfa +'.txt_mesh')
+    phi = np.genfromtxt(path + '/phi_' + alfa + '.csv', delimiter=',')
+    C = np.genfromtxt(path + '/C_' + alfa + '.csv', delimiter=',')
+    theta = np.genfromtxt(path + '/theta_' + alfa + '.csv',
                       delimiter=',')
-alfa = 6
 
-(u, v) = velocity(alfa, C, mach_inf, theta, mallaNACA, phi, v_inf)
-(cp, p) = pressure(u, v, v_inf, d_inf, gamma, p_inf, p0, d0, h0)
-(psi, mach) = streamlines(u, v, gamma, h0, d0, p, mallaNACA)
-(L, D) = lift_n_drag(mallaNACA, cp, alfa, 1)
-print("L = " + str(L))
-print("D = " + str(D))
+    (u, v) = velocity(int(alfa), C, mach_inf, theta, mallaNACA, phi, v_inf)
+    (cp, p) = pressure(u, v, v_inf, d_inf, gamma, p_inf, p0, d0, h0)
+    (psi, mach) = streamlines(u, v, gamma, h0, d0, p, mallaNACA)
+    (L, D) = lift_n_drag(mallaNACA, cp, int(alfa), 1)
+    cps[:, j] = cp[:, 0]
+    j += 1
+    print('alfa = ' + alfa)
+    print("L = " + str(L))
+    print("D = " + str(D))
+
+    # plt.figure('potential')
+    # plt.contour(mallaNACA.X, mallaNACA.Y, phi, 95, cmap=map_)
+    # plt.plot(mallaNACA.X[:, 0], mallaNACA.Y[:, 0], 'k')
+    # plt.plot(mallaNACA.X[:, -1], mallaNACA.Y[:, -1], 'k')
+    # plt.axis('equal')
+
+    # plt.figure('pressure')
+    # plt.plot(mallaNACA.X[:, 0], mallaNACA.Y[:, 0], 'k')
+    # plt.contourf(mallaNACA.X, mallaNACA.Y, cp, 180, cmap=map_)
+    # plt.colorbar()
+    # plt.axis('equal')
+
+    # plt.figure('streamlines')
+    # plt.plot(mallaNACA.X[:, 0], mallaNACA.Y[:, 0], 'k')
+    # plt.contour(mallaNACA.X, mallaNACA.Y, np.real(psi), 195, cmap=map_)
+    # plt.axis('equal')
+
+    # plt.draw()
+    # plt.show()
+    # limits = [[-20.5, 20.5, -20.5, 20.5], [-1.25, 1.25, -0.8, 0.8]]
+    # for limit in limits:
+    #     fig = plt.figure('malla_aspect')
+    #     ax = fig.add_subplot(1, 1, 1)
+    #     ax.set_xlim([limit[0], limit[1]])
+    #     ax.set_ylim([limit[2], limit[3]])
+    #     ax.set_aspect('equal')
+    #     ax.plot(mallaNACA.X[:, 0], mallaNACA.Y[:, 0], 'k', linewidth=1.9)
+    #     ax.plot(mallaNACA.X[:, -1], mallaNACA.Y[:, -1], 'k', linewidth=1.9)
+
+    #     mesh_ = plt.contour(mallaNACA.X, mallaNACA.Y, psi, 295,
+    #                         cmap=map_)
+    #     # plt.colorbar(mesh_)
+    #     plt.show()
+    #     plt.draw()
 
 
-# cps = np.zeros((95, 8))
-# j = 0
-#
+
+
 # path = '/home/cardoso/'
 # # direcs = ['four-', 'two-', 'zero', 'two', 'four', 'six', 'eight', 'ten']
-# direcs = ['four-', 'two', 'ten']
+# direcs = ['-4', '0', '4', '8']
 # for direc in direcs:
-#     mallaNACA = helpers.from_txt_mesh(filename='./potential_2412/' + direc
+#     mallaNACA = helpers.from_txt_mesh(filename=path + direc
 #                                       + '/mallaNACA.txt_mesh')
 #     phi = np.genfromtxt('./potential_2412/' + direc + '/phi.csv', delimiter=',')
 #     # f = open("./potential_2412/five/C.csv", "r")
@@ -104,18 +145,18 @@ x_perf = perfil.x
 y_perf = perfil.y
 cp_perf =cp[:, 0]
 
-# plt.figure('perf')
-# plt.plot(x_perf, y_perf * 7.5, 'k')
-# for j in range(3):
-#     plt.plot(x_perf[1:-1], cps[1:-1, j], label=(direcs[j]))
-#     # plt.plot(x_perf[1: points // 2 + 1], cps[1: points // 2 + 1, j], label=(direcs[j]))
-#     # plt.plot(x_perf[points // 2 + 1 :], cps[points // 2 + 1 :, j], label=(direcs[j]))
-#     # plt.plot(x_perf[points // 2 + 1:-1], cp_perf[points // 2 + 1:-1], 'k', label='extrados')
-# plt.legend(loc='lower right')
-# # plt.axis('equal')
-# plt.draw()
-# plt.show()
-# exit()
+plt.figure('perf')
+plt.plot(x_perf, y_perf * 8.5, 'k')
+for j in range(4):
+    plt.plot(x_perf[1:-1], cps[1:-1, j], label=(alfas[j]))
+    # plt.plot(x_perf[1: points // 2 + 1], cps[1: points // 2 + 1, j], label=(direcs[j]))
+    # plt.plot(x_perf[points // 2 + 1 :], cps[points // 2 + 1 :, j], label=(direcs[j]))
+    # plt.plot(x_perf[points // 2 + 1:-1], cp_perf[points // 2 + 1:-1], 'k', label='extrados')
+plt.legend(loc='lower right')
+# plt.axis('equal')
+plt.draw()
+plt.show()
+exit()
 
 mallaNACA.plot()
 
@@ -128,13 +169,13 @@ for limit in limits:
     ax.set_ylim([limit[2], limit[3]])
     ax.set_aspect('equal')
     # plt.axis('equal')
-    # ax.plot(mallaNACA.X, mallaNACA.Y, 'k', linewidth=0.5)
+    # ax.plot(mallaNACA.X, mallaNACA.Y, 'b', linewidth=1.5)
     ax.plot(mallaNACA.X[:, 0], mallaNACA.Y[:, 0], 'k', linewidth=1.9)
     ax.plot(mallaNACA.X[:, -1], mallaNACA.Y[:, -1], 'k', linewidth=1.9)
 
     # for i in range(mallaNACA.M):
-    #     ax.plot(mallaNACA.X[i, :], mallaNACA.Y[i, :], 'k', linewidth=0.5)
-    mesh_ = plt.contour(mallaNACA.X, mallaNACA.Y, psi, 495,
+    #     ax.plot(mallaNACA.X[i, :], mallaNACA.Y[i, :], 'b', linewidth=1.5)
+    mesh_ = plt.contour(mallaNACA.X, mallaNACA.Y, phi, 195,
                            cmap=map_)
     #plt.colorbar(mesh_)
     plt.show()
