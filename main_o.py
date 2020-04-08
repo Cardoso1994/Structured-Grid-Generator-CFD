90f43575aa00842d22fe7fbd1d719e5357065086
Marco Cardoso <marco@vivoxie.com>
2020-04-08 11:42:27 -0500

    Se logra vectorizacion de metodo de Poisson.
    Se vectorizan todas las ecuaciones, con lo que se logro quitar los 2 for
    loops anidados.
    En el corte, ya no se calculan los valores de los puntos para Y,
    UNICAMENTE para X.
    
    Mejora en el tiempo de ejecución BRUTAL, dias vs MINUTOS (for loops
    anidados vs vectorizado).

--- main_o.py
+++ main_o.py
@@ -26,11 +26,13 @@ eje "XI"
 en el caso de malla tipo O, coincide con el número de puntos del perfil
 '''
 N = 335
-union = 29
+union = 49
 
 # points = 11
 airfoil_points = 335 # 499
 airfoil_points = 859
+airfoil_points = 259
+airfoil_points = 749
 
 if malla == 'C':
     points = airfoil_points // 3  # * 2
@@ -59,7 +61,9 @@ print('M = ' + str(mallaNACA.M))
 print('N = ' + str(mallaNACA.N))
 
 # mallaNACA.gen_Poisson(metodo='SOR', omega=0.7, aa=245, cc=7.4, linea_eta=0)
-mallaNACA.gen_Poisson(metodo='SOR', omega=0.7, aa=145, cc=3.7, linea_eta=0)
+# mallaNACA.gen_Poisson(metodo='SOR', omega=0.7, aa=145, cc=3.7, linea_eta=0)
+# mallaNACA.gen_Poisson_v(metodo='SOR', omega=0.7, aa=12.5, cc=5, linea_eta=0)
+mallaNACA.gen_Poisson_v(metodo='SOR', omega=0.7, aa=230.5, cc=7.4, linea_eta=0)
 
 mallaNACA.to_su2('/home/desarrollo/garbage/mesh_o.su2')
 mallaNACA.to_txt_mesh('/home/desarrollo/garbage/mesh_o.txt_mesh')
